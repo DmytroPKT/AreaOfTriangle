57bcfe4f6f306b72a828a8af25ee78411e556599
DmytroPKT <demetriusz.pk@gmail.com>
2022-09-13 17:43:21 +0300

    Created hometask 4
    Hometask 4

---
+++ main.py
@@ -0,0 +1,4 @@
+triangle_side_a = int(input('Set triangle side size: '))
+triangle_height = int(input('Set triangle height length: '))
+triangle_square = int(triangle_side_a*triangle_height)/2
+print(triangle_square)
