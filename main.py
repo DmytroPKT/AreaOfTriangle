273451bdbdaa6e7d840596c9fd5f8ef43b90a5a4
DmytroPKT <demetriusz.pk@gmail.com>
2022-09-13 18:04:57 +0300

    Update 2
    added exception

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-triangle_side_a = float(input('Set triangle side size: '))
-triangle_height = float(input('Set triangle height length: '))
-triangle_square = float(triangle_side_a*triangle_height)/2
-print(triangle_square)
+try:
+    triangle_side_a = float(input('Set triangle side size: '))
+    triangle_height = float(input('Set triangle height length: '))
+    triangle_square = float(triangle_side_a*triangle_height)/2
+    print(triangle_square)
+# Error info
+except ValueError:
+    print('Do not enter letters or symbols. Please, restart the program and enter number')
