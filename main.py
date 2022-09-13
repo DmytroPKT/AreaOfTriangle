70fe1be9dbd314d313bbb06f3d3409ff186b5377
DmytroPKT <demetriusz.pk@gmail.com>
2022-09-13 18:28:24 +0300

    text update

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 try:
-    triangle_side_a = float(input('Set triangle side size: '))
-    triangle_height = float(input('Set triangle height length: '))
-    triangle_square = float(triangle_side_a*triangle_height)/2
-    print(triangle_square)
+    triangle_side_a = float(input('Enter triangle side A length (centimeters): '))  # Triangle side A length input
+    triangle_height = float(input('Enter triangle height length (centimeters): '))    # Triangle height length input
+    triangle_square = float((triangle_side_a*triangle_height)/2)    # S=(a*h)/2
+    print(f'Area of triangle is {triangle_square} sq.cm')
 # Error info
 except ValueError:
     print('Do not enter letters or symbols. Please, restart the program and enter number')
