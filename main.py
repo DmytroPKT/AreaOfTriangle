94800baf669e328605246161b456f46b5aaf585b
DmytroPKT <demetriusz.pk@gmail.com>
2022-09-13 17:46:16 +0300

    Update1
    int to float

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-triangle_side_a = int(input('Set triangle side size: '))
-triangle_height = int(input('Set triangle height length: '))
-triangle_square = int(triangle_side_a*triangle_height)/2
+triangle_side_a = float(input('Set triangle side size: '))
+triangle_height = float(input('Set triangle height length: '))
+triangle_square = float(triangle_side_a*triangle_height)/2
 print(triangle_square)
